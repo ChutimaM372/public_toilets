1087392f1241231a2455e5abacaa6fbc4072ea8b
Chutima Mueanprasat <mueanprasat_c@silpakorn.edu>
2023-09-27 22:38:49 +0700

    Initial commit

---
+++ lib/repositories/toilet_repository.dart
@@ -0,0 +1,12 @@
+import 'package:public_toilets/models/toilet.dart';
+
+class ToiletRepository{
+    static List<Toilet> toilets = [
+    Toilet(name: 'ห้องนํ้า 1', point: 1.9, distance: 150),
+    Toilet(name: 'ห้องนํ้า 2', point: 4.1, distance: 450),
+    Toilet(name: 'ห้องนํ้า 3', point: 5.0, distance: 150),
+    Toilet(name: 'ห้องนํ้า 4', point: 4.1, distance: 250),
+    Toilet(name: 'ห้องนํ้า 5', point: 2.1, distance: 50),
+
+  ];
+}
---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,116 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:public_toilets/models/toilet.dart';
+import 'package:public_toilets/repositories/toilet_repository.dart';
+import 'package:public_toilets/screens/home/toilet_list_item.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _toiletNameController = TextEditingController();
+  final _toiletPointController = TextEditingController();
+  final _toiletDistanceController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Public Toilets')),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: ToiletRepository.toilets.length,
+              itemBuilder: (ctx, i) {
+                Toilet toilet = ToiletRepository.toilets[i];
+
+                return ToiletListItem(
+                  toilet: toilet,
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _toiletNameController,
+                    decoration: InputDecoration(
+                      hintText: 'ชื่อห้องนํ้า',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 3, color: Colors.blue
+                        ),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _toiletPointController,
+                          decoration: InputDecoration(
+                            hintText: 'ให้คะแนน',
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3, color: Colors.blue
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          controller: _toiletDistanceController,
+                          decoration: InputDecoration(
+                            hintText: 'ระยะทาง',
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  width: 3, color: Colors.blue
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          var toiletName = _toiletNameController.text;
+                          var toiletPoint = double.parse(_toiletPointController.text);
+                          var toiletDist = double.parse(_toiletDistanceController.text);
+                          var toilet = Toilet(name: toiletName, point: toiletPoint, distance: toiletDist);
+
+                          setState(() {
+                            ToiletRepository.toilets.add(toilet);
+                          });
+                          _toiletNameController.clear();
+                          _toiletPointController.clear();
+                          _toiletDistanceController.clear();
+                        },
+                        child: Text('Add'),
+                      )
+
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
